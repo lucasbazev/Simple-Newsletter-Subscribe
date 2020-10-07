8b41757395a9984332b28c2077e522beade29fb6
Lucas Azevedo <lucasbazev@gmail.com>
2020-10-07 17:53:38 -0300

    Mudanças no HTML e JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.querySelector('button').onclick = function () {
-    const content = document.querySelector('#content')
+document.querySelector('input[type="submit"]').onsubmit = function (event) {
+    event.preventDefault();
+
+    textoConfirmacao();
+}
+
+function textoConfirmacao() {
+    onst content = document.querySelector('#content')
     .innerHTML = `
         <h1>Sucesso!</h1>
         <p>O cronograma atual da newsletter é <b>todos os dias da semana às 11h da manhã.</b> Nos vemos por lá!</p>
